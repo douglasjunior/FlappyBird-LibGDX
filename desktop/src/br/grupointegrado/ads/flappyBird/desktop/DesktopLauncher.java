26054be8c41dc93ef0b55b37cb47c3ce89fed892
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2015-09-28 19:59:50 -0300

    commit inicial

---
+++ desktop/src/br/grupointegrado/ads/flappyBird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.grupointegrado.ads.flappyBird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.grupointegrado.ads.flappyBird.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
